500651ea3a12e597075d209556aa6908bcbd7a2c
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-08-01 08:47:08 +0545

    set up

--- care/src/App.jsx
+++ care/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const router = createBrowserRouter([
+
+    {
+      path: "/",
+      element: <><Navbar /> <Home /></>
+    },
+    {
+      path: "/login",
+      element: <><Navbar /> <Login /></>
+    },
+    {
+      path: "/about",
+      element: <><Navbar /> <About /></>
+    },
+ {
+      path: "/user/:username",
+      element: <><Navbar /> <User/></>
+    },
+
+  ])
 
   return (
     <>
+    
+      <RouterProvider router={router} />  
     </>
   )
 }
---
+++ care/src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const navbar = () => {
+    return (
+        <><div className="parent">
+            <div>
+                <div className="logo"></div>
+            </div>
+            <nav>
+                <NavLink className={(e) => { return e.isActive ? "black" : "" }} to="/"><li>Home</li></NavLink>
+                <NavLink className={(e) => { return e.isActive ? "black" : "" }} to="/about"><li>About</li></NavLink>
+                <NavLink className={(e) => { return e.isActive ? "black" : "" }} to="/contact"><li>contact
+                </li></NavLink>
+            </nav>
+            <div className="account">
+                <img src="" alt="" />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default navbar
